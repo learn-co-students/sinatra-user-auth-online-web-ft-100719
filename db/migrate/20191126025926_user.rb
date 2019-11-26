1354fb3d3321bf436aebf796d79df039e8ce9358
stef-codes <stefcodes@gmail.com>
2019-11-25 20:38:43 -0800

    Done.

--- db/migrate/20191126025926_user.rb
+++ db/migrate/20191126025926_user.rb
@@ -1,13 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
-  def up
+  def change
     create_table :users do |t|
       t.string :name
       t.string :email
       t.string :password
     end
-  end
- 
-  def down
-    drop_table :users
   end
 end
